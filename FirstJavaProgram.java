40889d03dde3bbb542cf86b547b66715e6aeb10a
Felix <felix-mayer@gmx.de>
2021-06-10 16:53:25 +0200

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+	public static void main(String[] args){
+		System.out.println("My name is James");
+		System.out.println("I am 207 years old");
+		System.out.println("My hometown is Austin, Texas");
+	}
+}
